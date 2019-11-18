f2971338bc8185f1db9689b712a238af818b06d5
Ccoleman789 <courtney789@gmail.com>
2019-11-17 23:33:15 -0600

    playing with the automatic script
    script still needs some work.
    1. still having to reset wd
    2. position prompt may be in wrong order?
    3. abline now printing and producing a txt with numbers.  We should add the 1% threshold too
    
    Co-Authored-By: Hunter Bartelt <hunterbartelt@users.noreply.github.com>

--- QTL_mapping/RQTL.R
+++ QTL_mapping/RQTL.R
@@ -10,9 +10,9 @@ library(rcompanion)
 colNum <- readline("Column Number: ") #Column for pheno of interest
 colNum <- as.numeric(colNum) #convert to number
 colNam <- readline("Column Name: ") #name of pheno of interest
-
-runs <- 0
-for(x in 0:1){
+setwd("C:/Users/Courtney/Desktop/GitHub/Aim-3/QTL_mapping")
+runs <- 1
+for(x in 1:2){
   naNames <- c("NA","--")
   if(runs == 0){
     fileName <- "FemaleMap.csv" #Mapping table file
@@ -43,42 +43,44 @@ for(x in 0:1){
   dev.off()
 
   #Compute statistically significant values
-  #TraitCIM1000 <- cim(Map,pheno.col = colNum,method = "hk",map.function = "kosambi",n.perm = 1000) # Function will take a moment to run, determines LOD value
-  #png(filename = "LOD_Frequency.png")
-  #plot(TraitCIM1000,col = 'green') #show distribution of lod scores
-  #dev.off()
-  #LOD5 <- summary(TraitCIM1000)[1]
-  #write.csv(TraitCIM1000,file = "TraitCIM1000.csv")
+  TraitCIM1000 <- cim(Map,pheno.col = colNum,method = "hk",map.function = "kosambi",n.perm = 1000) # Function will take a moment to run, determines LOD value
+  png(filename = "LOD_Frequency.png")
+  plot(TraitCIM1000,col = 'green') #show distribution of lod scores
+  dev.off()
+  LOD5 <- summary(TraitCIM1000)[1]
+  capture.output(LOD5,file="lod_cutoff.txt")
+  write.csv(TraitCIM1000,file = "TraitCIM1000.csv")
 
   #Deterime LOD Values
 
   TraitCIM<-cim(Map,pheno.col = colNum,method = "hk",map.function = "kosambi") #Generate LOD Values
   png(filename = "LOD_vs_Genetic_Position.png")
   plot(TraitCIM) #show LOD graph
-  #abline(h = LOD5,col = "blue") #overlay statistically significant line
+  abline(h = LOD5,col = "blue") #overlay statistically significant line
   dev.off()
   print("If nothing shows up, the values are below the LOD5 score") #warning for no peaks
   write.csv(TraitCIM,file = "TraitCIM.csv")
+  capture.output(summary(TraitCIM),file = "TraitCIM_Summary.txt")
 
   #Chromosomal Analysis
-  cont = 'Y' #Run loop on first run through
-  while(cont == 'Y'){
+  cont = 'y' #Run loop on first run through
+  while(cont == 'y'){
     chr <- as.numeric(readline("Chromosome? ")) #collect chromosome to look at
     png(filename = "LOD_vs_Genetic_Position_Chr_of_interest.png")
     plot(TraitCIM, chr = chr) #Plot chr LOD scores
-    #abline(h = LOD5, col = "blue") #Statistically Significant
+    abline(h = LOD5, col = "blue") #Statistically Significant
     dev.off()
+    bay <- scanone(Map, method = 'hk') #scan genome with sQTL model
+    capture.output(bayesint(bay, chr = chr, prob=0.95, expandtomarkers=TRUE),file = paste("bayesin_",chr,".txt", sep = "")) #calculate bayesian interval
+    capture.output(lodint(bay, chr = chr,expandtomarkers = TRUE ),file = paste("lodint_",chr,".txt", sep = "")) #calculate LOD Support interval
     cont = readline("Would you like to investigate another chromosome? (Y/N): ") #Y if multiple chromosomes are to be investigated, saves last chromosome investigated
-}
-
-  bay <- scanone(Map, method = 'hk') #scan genome with sQTL model
-  capture.output(bayesint(bay, chr = chr, prob=0.95, expandtomarkers=TRUE),file = "bayesint.txt") #calculate bayesian interval
-  capture.output(lodint(bay, chr = chr,expandtomarkers = TRUE ),"lodint.txt") #calculate LOD Support interval
-
-  #TODO, automate pos value
-  #fitqtl analysis
-  qtl <- makeqtl(Map, chr = chr, pos = 20.05, what="prob") #pulls genotype probabilities
-  fitqtl <- fitqtl(Map, pheno.col=colNum, qtl = qtl, covar=NULL,method= "hk",model="normal",dropone=TRUE, get.ests=TRUE,run.checks=TRUE,tol=1e-4, maxit=1000, forceXcovar=FALSE) #sees how well our data fits a given formula If you see "error: object of type 'closure' is not subsettable", remove "formula," from the fitqtl argument list.
-  capture.output(summary(fitqtl),"fitqtl.txt") #Print results of the analysis
+    #TODO, automate pos value
+    #fitqtl analysis
+    pos <- readline("Position? ")
+    qtl <- makeqtl(Map, chr = chr, pos = pos, what="prob") #pulls genotype probabilities
+    fitqtl <- fitqtl(Map, pheno.col=colNum, qtl = qtl, covar=NULL,method= "hk",model="normal",dropone=TRUE, get.ests=TRUE,run.checks=TRUE,tol=1e-4, maxit=1000, forceXcovar=FALSE) #sees how well our data fits a given formula If you see "error: object of type 'closure' is not subsettable", remove "formula," from the fitqtl argument list.
+    capture.output(summary(fitqtl),file = paste("fitqtl_",chr,".txt", sep = "")) #Print results of the analysis
+  }
+  setwd("C:/Users/Courtney/Desktop/GitHub/Aim-3/QTL_mapping")
   runs <- runs + 1
 }
