7c4faaf5322af0b6fb5191ca275529a8435ab0b9
zoemigicovsky <zoe.migicovsky@dal.ca>
2020-01-10 15:09:44 -0400

    species to include

--- QTL_mapping/docs/PCA/genevaionomics/geneva_ionomics_code.R
+++ QTL_mapping/docs/PCA/genevaionomics/geneva_ionomics_code.R
@@ -8,3 +8,13 @@ library(ggbeeswarm)
 #Load ionomics data
 
 ionomics_data <- read_csv('QTL_mapping/docs/PCA/genevaionomics/160822_Data.Chitwood705.csv')
+
+#North American species to include:
+#Vitis_palmata
+#Vitis_aestivalis
+#Vitis_vulpina
+#Vitis_acerifolia
+#Vitis_rupestris
+#Vitis_cinerea
+#Vitis_labrusca
+#Vitis_riparia
